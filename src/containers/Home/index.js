af45ec8a2af0a33ae588689c0e4180f7766ee97f
thejoker <adnanahmed@outlook.com>
2019-07-08 15:33:15 +0100

    Updated

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -4,8 +4,12 @@ import BaseContainer from 'components/BaseContainer';
 import { Link } from 'react-router-dom';
 
 const Project = () => (
-  <BaseContainer title="Person Name" subtitle="Full Stack Developer">
-    <Typography variant="h5">This is the home page</Typography>
+  <BaseContainer
+    variant="h1"
+    title="Forename Surname"
+    subtitle="Full Stack Developer"
+  >
+    <Typography variant="p">This is the home page</Typography>
     <Typography variant="h6">
       <Link to="/project/app">Learn more...</Link>
     </Typography>
